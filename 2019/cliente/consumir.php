1d0d46ee5bfaaf37a6e021f384eef526088e2c7b
cait123 <neverstopdreaming__@hotmail.com>
2019-09-01 00:20:37 -0600

    first commit

---
+++ 2019/cliente/consumir.php
@@ -0,0 +1,20 @@
+<?php
+require_once('../lib/nusoap.php');
+// direccion del WSDL URL del servidor cambiar por la ip o nombre del server
+$wsdl = "http://localhost:8080/DW/2019/servicio.php?wsdl";
+
+// crear el objeto de cliente
+$client = new nusoap_client($wsdl, 'wsdl');
+$err = $client->getError();
+if ($err) {
+   // Si hay error despliga el tag html del mismo
+   echo '<h2>Error en la construccion</h2>' . $err;
+
+   // At this point, you know the call that follows will fail
+   exit();
+}
+
+// Call the hello method
+$result1=$client->call('obtenerdatos', array('nombre'=>'KATHYYYY'));
+print_r($result1).'¡RESULTADO!\n';
+?>
